efa12a9887c008f8596bc21fd8897785a71971da
RomanL91 <dev_roman91@mail.ru>
2025-01-16 01:33:11 +0500

    схемы для телеф номеров

---
+++ app_phone_numbers/schemas.py
@@ -0,0 +1,25 @@
+from uuid import UUID
+
+from pydantic import BaseModel, Field, field_validator
+
+from app_phone_numbers import utils
+
+
+class PhoneNumberRequest(BaseModel):
+    phone_number: str = Field(
+        ...,
+        pattern=r"^(?:\+7|8)\d{10}$",  # Номер должен начинаться с +7 или 8 и содержать 10 цифр после этого
+        description="Телефонный номер должен начинаться с +7 или 8 и содержать 10 цифр. Пример: +71234567890 или 81234567890",
+    )
+
+    @field_validator("phone_number")
+    def validate_phone_number(cls, value):
+        # некоторая логика (например, нормализация номера)
+        return utils.normalize_phone(value)
+
+
+class PhoneNumberResponse(BaseModel):
+    id: UUID = Field(
+        ...,
+        description="Уникальный идентификатор записи телефонного номера",
+    )
