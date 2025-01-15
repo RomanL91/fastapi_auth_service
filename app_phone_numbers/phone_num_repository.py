94a76efdb957b9318929989bb7e13810b9363f30
RomanL91 <dev_roman91@mail.ru>
2025-01-16 02:39:31 +0500

    метод для репозитория PhoneNumberRepository для подгрузки связанной инфы о пользователях

--- app_phone_numbers/phone_num_repository.py
+++ app_phone_numbers/phone_num_repository.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+
 from core.BASE_repository import SQLAlchemyRepository
 
 from app_phone_numbers.models import PhoneNumber
@@ -5,3 +8,15 @@ from app_phone_numbers.models import PhoneNumber
 
 class PhoneNumberRepository(SQLAlchemyRepository):
     model = PhoneNumber
+
+    async def get_obj_with_user(self, phone_number: str) -> None | PhoneNumber:
+        """
+        Метод, который подгружает и PhoneNumber, и связанного пользователя.
+        """
+        stmt = (
+            select(PhoneNumber)
+            .where(PhoneNumber.phone_number == phone_number)
+            .options(joinedload(PhoneNumber.user))
+        )
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
