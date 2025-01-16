020d7ea8f33c3977663731fd5165a9141d28b76b
RomanL91 <dev_roman91@mail.ru>
2025-01-16 23:47:51 +0500

    метод SMSCodeService для проверки существования смс кода по определенным фильтрам

--- app_sms/sms_service.py
+++ app_sms/sms_service.py
@@ -1,1 +1,27 @@
+from uuid import UUID
 
+from core.BASE_unit_of_work import UnitOfWork
+
+from app_sms.models import SMSCode
+
+
+class SMSCodeService:
+    def __init__(self, uow: UnitOfWork):
+        self.uow = uow
+
+    async def chec_sms_code(
+        self,
+        code: str,
+        phone_id: UUID,
+    ) -> SMSCode:
+        async with self.uow as uow:
+            sms = await uow.sms.get_unused_code_for_phone(
+                code,
+                phone_id,
+            )
+            if sms:
+                sms.is_used = True
+                sms.is_active = False
+                await uow.commit()
+                return sms
+            return None
