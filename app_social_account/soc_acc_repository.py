2191c10952768cbb1f861a4a3aa343310f157fb7
RomanL91 <dev_roman91@mail.ru>
2025-01-17 09:17:02 +0500

    переопределил метод get_obj в SocialAccountRepository чтобы всегда получать 1 результат

--- app_social_account/soc_acc_repository.py
+++ app_social_account/soc_acc_repository.py
@@ -1,3 +1,7 @@
+from sqlalchemy.engine import Result
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy import select
+
 from core.BASE_repository import SQLAlchemyRepository
 
 from app_social_account.models import SocialAccount
@@ -5,3 +9,11 @@ from app_social_account.models import SocialAccount
 
 class SocialAccountRepository(SQLAlchemyRepository):
     model = SocialAccount
+
+    async def get_obj(self, **filter_by):
+        stmt = select(self.model).filter_by(**filter_by).limit(1)
+        try:
+            res: Result = await self.session.execute(stmt)
+            return res.scalar_one()
+        except NoResultFound:
+            return None
