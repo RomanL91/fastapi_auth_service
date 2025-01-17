1d1a92ec231cb744da893b7dca01ff7a923709f9
RomanL91 <dev_roman91@mail.ru>
2025-01-17 09:15:25 +0500

    подключение роутера для авторизации через гугл

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
 from api_v1.auth_phone_sms.views import router as phone_router
+from api_v1.oath2_google.views import router as oath2_google_router
 
 router = APIRouter()
 
 router.include_router(router=phone_router, prefix="/auth_phone")
-# router.include_router(router=oath2_google_router, prefix="/auth_user")
+router.include_router(router=oath2_google_router, prefix="/auth_user")
 # router.include_router(router=oauth2_vk_router, prefix="/auth_user")
 # router.include_router(router=auth_phone_router, prefix="/auth_user")
 # router.include_router(router=user_router, prefix="/user")
