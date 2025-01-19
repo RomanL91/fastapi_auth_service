f73f2d09d35756ae7169e7b02103f40be429fe3b
RomanL91 <dev_roman91@mail.ru>
2025-01-19 14:37:46 +0500

    для endpoint по обновлению токена работает через headrs

--- api_v1/jwt/views.py
+++ api_v1/jwt/views.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, status, HTTPException
 
 
-from api_v1.api_dependencies import UOF_Depends
+from api_v1.api_dependencies import UOF_Depends, Refresh_JWT_Depends
 
-from app_jwt.schemas import JWTRequestRefresh, JWTSchema
 from app_jwt.utils import jwt_util
 from app_jwt.jwt_service import JWTService
 
@@ -14,18 +13,19 @@ router = APIRouter(tags=["token"])
 @router.post(
     "/refresh",
     status_code=status.HTTP_201_CREATED,
-    response_model=JWTSchema,
+    # response_model=JWTSchema,
     summary="Обновить ключ доступа.",
     description="""
         **Через данный endpoint можно получить ключ доступа `типа access` в обмен на ключ доступа `типа refresh`.**
     """,
 )
-async def refresh(uow: UOF_Depends, token: JWTRequestRefresh):
-    data_payload_token = token.model_dump()
-    jwt_service = JWTService(uow=uow)
-
-    if data_payload_token:
-        user_id = data_payload_token["token"]["user_id"]
+async def refresh(
+    uow: UOF_Depends,
+    token: Refresh_JWT_Depends,
+):
+    user_id = token.get("user_id", None)
+    if user_id:
+        jwt_service = JWTService(uow=uow)
 
         access_token = await jwt_service.create_and_store_access(
             user_id=user_id,
