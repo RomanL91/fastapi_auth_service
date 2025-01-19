4679e679ea02db6bf9ba2221cd50c44f121aaf2c
RomanL91 <dev_roman91@mail.ru>
2025-01-19 14:41:16 +0500

    метод UserRepository для получения пользователя по ID + связанной информацией

--- app_users/users_repository.py
+++ app_users/users_repository.py
@@ -1,3 +1,8 @@
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
 from core.BASE_repository import SQLAlchemyRepository
 
 from app_users.models import User
@@ -5,3 +10,27 @@ from app_users.models import User
 
 class UserRepository(SQLAlchemyRepository):
     model = User
+
+    async def get_user_by_id(self, user_id: UUID) -> User | None:
+        """
+        Получает пользователя по user_id с загруженными связями.
+
+        Args:
+            user_id (UUID): Идентификатор пользователя.
+
+        Returns:
+            User | None: Объект пользователя с загруженными связями или None, если не найден.
+        """
+        stmt = (
+            select(User)
+            .options(
+                selectinload(User.phone),  # Загрузка связи PhoneNumber
+                selectinload(User.social_accounts),  # Загрузка связи SocialAccount
+                # selectinload(User.tokens),  # Загрузка связи JWToken
+                # selectinload(User.sms_codes),  # Загрузка связи SMSCode
+            )
+            .where(User.id == user_id)
+        )
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()
+        return user
