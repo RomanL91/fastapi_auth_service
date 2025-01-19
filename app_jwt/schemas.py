b035a8d01e1adcd47b789e39bc281b6e302202d2
RomanL91 <dev_roman91@mail.ru>
2025-01-19 14:38:50 +0500

    удалил логику пидантик схем с валидацией и изменением типа ответа

--- app_jwt/schemas.py
+++ app_jwt/schemas.py
@@ -1,18 +1,10 @@
 from datetime import datetime
 
-from fastapi import status, HTTPException
-
-from typing_extensions import Annotated
-
 from pydantic import (
     BaseModel,
     ConfigDict,
     UUID4,
-    Field,
-    field_validator,
 )
-
-from app_jwt.utils import jwt_util
 
 
 class JWTSchema(BaseModel):
@@ -41,24 +33,3 @@ class JWTSchema(BaseModel):
 class JWTPairResponse(BaseModel):
     access: JWTSchema
     refresh: JWTSchema
-
-
-class JWTRequestRefresh(BaseModel):
-    token: Annotated[
-        str,
-        Field(
-            ...,
-            description="JWT токен",
-            example="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9...",
-        ),
-    ]
-
-    @field_validator("token")
-    def validate_token_and_extract_payload(cls, value):
-        payload_data = jwt_util.decode_jwt(value)
-        if payload_data["type"] == jwt_util.refresh_token_type:
-            return jwt_util.decode_jwt(value)
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid token type",
-        )
