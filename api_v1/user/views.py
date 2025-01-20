5b7df349ce2b798f01a98341920e316b3cc82b19
RomanL91 <dev_roman91@mail.ru>
2025-01-20 21:08:25 +0500

    endpoints для изменения пользователя + для добавления автара пользователю

--- api_v1/user/views.py
+++ api_v1/user/views.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import (
+    APIRouter,
+    status,
+    HTTPException,
+    Request,
+    UploadFile,
+    File,
+)
 
 from api_v1.api_dependencies import UOF_Depends, Access_JWT_Depends
 
@@ -6,7 +13,7 @@ from api_v1.api_dependencies import UOF_Depends, Access_JWT_Depends
 from app_users.user_service import UserService
 
 # === Schemas
-from app_users.schemas import UserSchema
+from app_users.schemas import UserSchema, UserUpdateSchema
 
 
 router = APIRouter(tags=["user"])
@@ -33,6 +40,61 @@ async def get_user_info(
         user_info = await user_service.get_user_info(user_id=user_id)
         # Отдаем информацию о пользователе
         return UserSchema.model_validate(user_info)
+    # Иначе выкидываем исключение
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Unexpected data retrieval error",
+    )
+
+
+@router.patch(
+    "/update",
+)
+async def update_user_info(
+    user_update: UserUpdateSchema,
+    uow: UOF_Depends,
+    token: Access_JWT_Depends,
+):
+    user_id = token.get("user_id", None)
+    if user_id:
+        # Инициализируем сервис и вызываем метод `get_user_info`
+        user_service = UserService(uow=uow)
+        updated_user = await user_service.update_user_info(user_id, user_update)
+        return UserSchema.model_validate(updated_user)
+    # Иначе выкидываем исключение
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Unexpected data retrieval error",
+    )
+
+
+@router.post(
+    "/avatar",
+    response_model=UserSchema,
+    summary="Обновить аватарку пользователя",
+    description="""
+        **Обновляет аватарку текущего пользователя.**
+        Загрузите файл изображения, и он будет сохранён на сервере.
+    """,
+    status_code=status.HTTP_200_OK,
+)
+async def update_user_avatar(
+    token: Access_JWT_Depends,
+    uow: UOF_Depends,
+    request: Request,
+    file: UploadFile = File(..., description="Новая аватарка пользователя"),
+):
+    user_id = token.get("user_id", None)
+
+    if user_id:
+        user_service = UserService(uow=uow)
+        user = await user_service.payload_avatar(
+            unique_name=user_id,
+            request=request,
+            file=file,
+        )
+        return user
+
     # Иначе выкидываем исключение
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
