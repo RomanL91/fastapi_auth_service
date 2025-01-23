afe46f906e444fce69d9a5db6592c625f266b500
RomanL91 <dev_roman91@mail.ru>
2025-01-23 21:00:45 +0500

    новая модель адреса пользователя

--- app_users/models.py
+++ app_users/models.py
@@ -1,8 +1,9 @@
-from typing import List
+from typing import List, Optional
 from datetime import datetime
 
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 from core.BASE_model import Base
 
@@ -57,3 +58,39 @@ class User(Base):
         back_populates="user",
         cascade="all, delete-orphan",
     )
+    points_balance: Mapped[Optional["UserPointsBalance"]] = relationship(  # type: ignore
+        "UserPointsBalance",
+        back_populates="user",
+        uselist=False,
+        cascade="all, delete-orphan",
+    )
+    addresses: Mapped[List["UserAddress"]] = relationship(
+        "UserAddress",
+        back_populates="user",
+        cascade="all, delete-orphan",
+    )
+
+
+class UserAddress(Base):
+    """
+    Адрес доставки пользователя
+    """
+
+    user_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True,
+    )
+    # Поля адреса
+    street_line1: Mapped[str] = mapped_column(String, nullable=False)
+    street_line2: Mapped[str | None] = mapped_column(String, nullable=True)
+    city: Mapped[str] = mapped_column(String, nullable=False)
+    state: Mapped[str | None] = mapped_column(String, nullable=True)
+
+    # Дополнительный номер телефона, указанный при доставке
+    phone: Mapped[str | None] = mapped_column(String, nullable=True)
+    is_default: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    # Связь с пользователем
+    user: Mapped["User"] = relationship("User", back_populates="addresses")
