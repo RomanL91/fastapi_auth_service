7ff25709838e3b2f0cd7a5e9124a139c501f114d
RomanL91 <dev_roman91@mail.ru>
2025-01-27 00:21:48 +0500

    подключение роутеров

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -5,6 +5,8 @@ from api_v1.oath2_google.views import router as oath2_google_router
 from api_v1.jwt.views import router as jwt_router
 from api_v1.user.views import router as user_router
 from api_v1.address.views import router as address_router
+from api_v1.wishlist_items.views import router as wishlist_ruoter
+from api_v1.viewed_prod.views import router as viewed_router
 
 router = APIRouter()
 
@@ -13,5 +15,6 @@ router.include_router(router=oath2_google_router, prefix="/auth_user")
 router.include_router(router=jwt_router, prefix="/token")
 router.include_router(router=user_router, prefix="/user")
 router.include_router(router=address_router, prefix="/address")
-# router.include_router(router=oauth2_vk_router, prefix="/auth_user")
+router.include_router(router=wishlist_ruoter, prefix="/wishlist")
+router.include_router(router=viewed_router, prefix="/viewed")
 # router.include_router(router=auth_phone_router, prefix="/auth_user")
